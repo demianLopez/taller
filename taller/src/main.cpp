70843cfc756cbfb92ddf00495d38b6fbffdaaf61
EscDemi <demianlopez1@gmail.com>
2014-08-21 16:22:15 -0300

    Creado main

---
+++ taller/src/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 21/8/2014
+ *      Author: EscDemi
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!! 2" << endl; // prints !!!Hello World!!!
+	return 0;
+}
+
+
