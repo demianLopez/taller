5b3e9bb6fe21acc1c4e9762c4c54694080d3bfc1
Xero-Hige <gaston.martinez.90@gmail.com>
2014-08-30 12:50:56 -0300

    Agregado un back para que no sea tan asqueroso de ver Separado el handler de eventos de teclado

--- jump_test.cpp
+++ jump_test.cpp
@@ -27,6 +27,26 @@ const float32 timeStep = 1 / 20.0; 	//Tiempo de la simulacion del mundo
 const int32 velocityIterations = 8;
 const int32 positionIterations = 3;
 
+void handle_keyboard_event(const SDL_Event& event, b2Body* full_body) {
+	//Keypress handle
+	switch (event.key.keysym.scancode) {
+	case SDL_SCANCODE_LEFT:
+		full_body->SetLinearVelocity(b2Vec2(-200, 0));
+		break;
+	case SDL_SCANCODE_RIGHT:
+		full_body->SetLinearVelocity(b2Vec2(200, 0));
+		break;
+	case SDL_SCANCODE_UP:
+		full_body->SetLinearVelocity(b2Vec2(0, -200));
+		break;
+	case SDL_SCANCODE_DOWN:
+		full_body->SetLinearVelocity(b2Vec2(0, 200));
+		break;
+	case SDL_SCANCODE_R:
+		full_body->SetLinearVelocity(b2Vec2(0, 0));
+	}
+}
+
 int main() {
 
 	//Inicializo los componentes de SDL
@@ -34,6 +54,11 @@ int main() {
 
 	//Creo una ventana
 	Window game_window = Window("Megaman NT Warriors", 1000, 1000, 0);
+
+	//background
+	Sprite back = Sprite("./res/backgrounds/1.png", game_window);
+	back.set_scaled_height(1000);
+	back.set_scaled_width(1000);
 
 	//Esto deberia ser una clase en si --------------------------
 	Sprite player = Sprite("./res/players/player1.png", game_window);
@@ -50,10 +75,19 @@ int main() {
 	b2Body* full_body = world.CreateBody(&base_body);
 	full_body->SetLinearVelocity(b2Vec2(100, 0));
 
+	b2CircleShape circleShape;
+	circleShape.m_p.Set(0, 0);
+	circleShape.m_radius = 4;
+	b2FixtureDef myFixtureDef;
+	myFixtureDef.shape = &circleShape;
+	myFixtureDef.density = 5;
+	full_body->CreateFixture(&myFixtureDef);
+
 	/*Ciclo principal*/
 	bool running = true;
 	while (running) {
 
+		back.draw(game_window);
 		player.draw(game_window);
 		game_window.render();
 		world.Step(timeStep, velocityIterations, positionIterations);
@@ -70,14 +104,12 @@ int main() {
 
 			case SDL_KEYDOWN:
 				//Keypress handle
-				break;
-
+				handle_keyboard_event(event, full_body);
 			}
 		}
 
 		SDL_Delay(20);
 
 	}
-
 
 }
