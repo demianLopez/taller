15ff6aa4165c9b38f74b8f2ab18b14138e7ccc4a
Maxi <maxipenia@hotmail.com>
2014-09-22 19:22:26 -0300

    Enloqueci viendo por que no compilaba. Creo que alguien borro el main..

---
+++ taller/src/main.cpp
@@ -0,0 +1,19 @@
+#include "SDL.h"
+#include <iostream>
+#include <Box2D/Box2D.h>
+#include <stdio.h>
+#include "SnowBross.h"
+#include "Jugador.h"
+#include "LectorJson.h"
+using namespace std;
+	int main(int argc, char *argv[]){
+	LectorJson* lector = new LectorJson();
+	lector->cargarEscenario("asf");
+	delete lector;
+	SnowBross *pE = new SnowBross("Snow Bross");
+	//pruebaEngine *pE = new pruebaEngine("Il");
+	pE->setMaxFPS(50);
+	pE->showFPS(true);
+	pE->start();
+	return 0;
+}
