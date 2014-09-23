badfd2ec09f3aeb06ae2af47117d1e9caafb7e07
demianlopez <demianlopez1@gmail.com>
2014-09-23 03:00:52 -0300

    Cambio cuerpo de personaje de cubo a hexagono

--- taller/src/Jugador.cpp
+++ taller/src/Jugador.cpp
@@ -1,26 +1,37 @@
 #include "Jugador.h"
-
+#include <math.h>
 // Inicializa Jugador.
 Jugador::Jugador(b2World * gameWorld) : Personaje(gameWorld) {
 	//Defino forma, posicion y tamanyo del body para el Personaje.
 
 	// TODO: permitir ponerlo en una posicion especifica.
+	b2Vec2* vertex = new b2Vec2[6];
+
+	double longX = 1.4f;
+	double longY = 1.8f;
+
+	double perimeter = 2 * 3.14f;
+	for (size_t i = 0; i < 6; i++) {
+		double angle = perimeter/6 * i;
+		vertex[i].Set(longX * cos(angle), longY * sin(angle));
+	}
+
 	b2BodyDef bodyDef;
 	bodyDef.type = b2_dynamicBody;
 	bodyDef.position.Set(5.0f, 10.0f);
 
 	body = gameWorld->CreateBody(&bodyDef);
 
-	b2PolygonShape dynamicBox;
+	b2PolygonShape polygon_shape;
+	polygon_shape.Set(vertex, 6);
 
-	double longX = 1.4f;
-	double longY = 1.8f;
 
-	dynamicBox.SetAsBox(longX, longY); //TODO: mejorar medidas.
+
+	//TODO: mejorar medidas.
 	this->size = b2Vec2(longX * 2.5, longY * 2);
 
 	b2FixtureDef fixtureDef;
-	fixtureDef.shape = &dynamicBox;
+	fixtureDef.shape = &polygon_shape;
 	fixtureDef.density = 1.5;
 	fixtureDef.friction = 0.05;
 
