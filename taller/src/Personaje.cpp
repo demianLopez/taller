0a6a37151084a1cbc5ebd6c2824cf6ba82cd07d1
Maxi <maxipenia@hotmail.com>
2014-09-23 04:34:28 -0300

    Borrada linea de mas.

--- taller/src/Personaje.cpp
+++ taller/src/Personaje.cpp
@@ -11,7 +11,7 @@ void Personaje::jump(){
 	if(!this->isOnAir()){
 		b2Vec2 currentVel = this->body->GetLinearVelocity();
 		this->body->SetLinearVelocity(b2Vec2(currentVel.x, movementSpeedY));
-		cout << movementSpeedY << endl;
+
 		//body->ApplyLinearImpulse(b2Vec2(0,body->GetMass() * 8), body->GetWorldCenter(), true);
 		//body->ApplyForceToCenter(b2Vec2(0,1006), true);
 
