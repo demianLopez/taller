fa2189b5f74b9b5f401a701372b6f4577fe70dd3
Xero-Hige <gaston.martinez.90@gmail.com>
2014-10-11 18:16:16 -0300

    Agregado Socket. Al igual que lo anterior compila pero no esta probado: Resultados no garantizados

---
+++ taller/common/Socket.h
@@ -0,0 +1,73 @@
+/**
+ Socket.h
+
+ Copyright 2014 Gaston Martinez Gaston.martinez.90@gmail.com
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses
+ */
+#ifndef SOCKET_H
+#define SOCKET_H
+
+#include <netdb.h>    //hostent
+#include <string.h>   //memset
+#include <unistd.h>   //close
+
+#include <string>
+
+//Definiciones para reducir codigo
+typedef struct in_addr in_addr_s;
+typedef struct sockaddr_in sockaddr_in_s;
+
+class _socket {
+protected:
+	int sockfd; //File descriptor del socket
+	int _port;  //Puerto asociado al socket
+	sockaddr_in_s _address; //Direccion de conexion
+
+protected:
+	_socket();
+
+public:
+	void close_port();
+	void shutdown_socket();
+	int get_port();
+};
+
+class Socket: public _socket {
+
+public:
+
+	Socket(int port, const std::string& host = "127.0.0.1");
+	Socket(int sockfd, sockaddr_in_s direccion);
+
+	int receive(char* buff, size_t bytes_to_read);
+	int send_message(const char* message, size_t message_len);
+
+};
+
+class SocketQueue: public _socket {
+
+public:
+	/**
+	 * Crea una nueva cola de socket no asociada a algun puerto
+	 */
+	SocketQueue();
+	void init(int port, in_addr_t address = INADDR_ANY);
+	void init(int port, const char* address);
+	void listen_connection(int backlog = 20);
+
+	Socket accept_connection();
+};
+
+#endif
