37a99be3b17fa0c6e9e3f76ccdf73835f41213e6
Xero-Hige <gaston.martinez.90@gmail.com>
2014-10-25 17:26:28 -0300

    Agrego el decript de los mensajes del lado del cliente

--- taller/example_client.cpp
+++ taller/example_client.cpp
@@ -5,6 +5,7 @@
 #include <chrono>
 #include <signal.h>
 
+#include "common/message_command.h"
 #include "server/accept_queue.h"
 #include "server/client_handler.h"
 
@@ -43,27 +44,93 @@ static void send_msj(Socket* socket, std::mutex* std_out_mutex) {
 	socket->close_port();
 }
 
+void handle_connection_error(Socket* socket) {
+	std::cerr << "Error de conexion con el cliente. Desconexion" << std::endl;
+	socket->shutdown_socket();
+}
+
 static void recv_msj(Socket* socket, std::mutex* std_out_mutex) {
 	signal(SIGPIPE, SIG_IGN);
 	while (socket->is_valid()) {
-		char message[100];
-		//Send from socket
-		int bytes_read = socket->receive(message, 25);
+		//Obtengo el comando
+		size_t command_len = sizeof(command_id_type_t);
+		char* command_buffer = new char [command_len];
+		int bytes_read = 0;
+		while(bytes_read < command_len){
+			int read = socket->receive(command_buffer+bytes_read, command_len-bytes_read);
+			if (read<0){
+				bytes_read = read;
+				break;
+			}
+			bytes_read += read;
+		}
 
 		if (bytes_read < 0) {
-			std::cerr << "Error de conexion con el cliente. Desconexion"
-					<< std::endl;
-			socket->shutdown_socket();
+			handle_connection_error(socket);
 			break;
 		}
 
-		message[bytes_read] = '\0';
+		command_id_type_t command = *((command_id_type_t*)command_buffer);
 
-		std::string mess = std::string(message);
+
+		//Obtengo la longitud de los parametros
+		size_t command_len_len = sizeof(command_len_type_t);
+		char* command_len_buffer = new char [command_len_len];
+		bytes_read = 0;
+		while(bytes_read < command_len_len){
+			int read = socket->receive(command_len_buffer+bytes_read, command_len_len-bytes_read);
+			if (read<0){
+				bytes_read = read;
+				break;
+			}
+			bytes_read += read;
+		}
+
+		if (bytes_read < 0) {
+			handle_connection_error(socket);
+			break;
+		}
+
+		command_len_type_t args_len = *((command_len_type_t*)command_len_buffer);
+
+
+		//Obtengo los parametros
+		char* args_buffer = new char [args_len+1];
+		bytes_read = 0;
+		while(bytes_read < args_len){
+			int read = socket->receive(args_buffer+bytes_read, args_len-bytes_read);
+			if (read<0){
+				bytes_read = read;
+				break;
+			}
+			bytes_read += read;
+		}
+
+		if (bytes_read < 0) {
+			handle_connection_error(socket);
+			break;
+		}
+
+		command_args_type_t args = *((command_args_type_t*)args_buffer);
+
+
+
+		//DEBUG
+
+		int a = command;
+		int b = args_len;
+		char* c = (char*)args;
+
+		c[bytes_read] = '\0';
+		std::string mess = std::string(c);
 
 		std_out_mutex->lock();
-		std::cout << "Somebody told me: " << mess << std::endl;
+		std::cout << "Comando: " << a << std::endl;
+		std::cout << "Arg len: " << b << std::endl;
+		std::cout << "Args   : " << mess << std::endl;
 		std_out_mutex->unlock();
+
+		//DEBUG
 
 	}
 	socket->close_port();
