9cb88d0427f8479c65a3e803deb7335b1a62a7c5
agustin <agustinalgo@yahoo.com>
2014-11-30 23:51:28 -0300

    Movimiento loco de monstruo

--- taller/server/World.cpp
+++ taller/server/World.cpp
@@ -493,7 +493,8 @@ void World::worldLoop(World * world) {
 
 		if(world->getPlayerList().size() > 0){
 			for(auto * e : world->getEnemyList()){
-				e->evaluateMovement(world->getPlayerList()[0]);
+				//e->evaluateMovement(world->getPlayerList()[0]);
+				e->movimientoLoco();
 				e->update();
 			}
 		}
